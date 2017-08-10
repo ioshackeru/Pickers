a5ede7809b64211c029561ddfc72e622feaf166d
TomerBu <tomer.bu@gmail.com>
2017-08-10 09:17:26 +0300

    Initial Commit

---
+++ DelegationDemos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DelegationDemos
+//
+//  Created by Tomer Buzaglo on 10/08/2017.
+//  Copyright © 2017 iTomerBu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
